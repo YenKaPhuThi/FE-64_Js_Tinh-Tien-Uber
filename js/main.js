b32ad1854383b11d96d5388618375eb1828b49b9
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-04-28 17:15:39 +0700

    [Js]: Check lenght and type value inputted

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-function handleSumCharging() {
-	document.getElementById("btncharging").addEventListener("click", function () {
+function handleSumCharge() {
+	document.getElementById("btncharge").addEventListener("click", function () {
 		var kmNumber      = document.getElementById("kmNumber");
 		var timeWaiting   = document.getElementById("timeWaiting");
 		var chargingBlock = document.getElementById("divThanhTien");
@@ -9,56 +9,73 @@ function handleSumCharging() {
 		kmNumberVal = kmNumber.value;
 		timeWaitingVal = timeWaiting.value;
 
-		kmNumber = parseFloat(kmNumberVal);
-		timeWaiting = parseFloat(timeWaitingVal);
+		// Checkt Km Number Value or Time Waiting is empty => don't do anything
+    // - This field is required
+    if (kmNumberVal.length == 0 || timeWaitingVal == 0) {
+			chargingBlock.style.display = "block";
+			chargingInfo.innerText = "Vui lòng không để trống!";
 
-		// Set uberX is checked as default
-		// - So number Payment is caculate following uberX
-		var kmNumberTypeBegin = 8000;
-		var kmNumberTypeMiddle = 12000;
-		var kmNumberTypeExtend = 10000;
+      return null;
+    }
 
-		var staticTime = timeWaiting * 2000;
-
-		var selectedUserType = "";
-		for (var i = 0; i < uberType.length; i++) {
-			if (uberType[i].checked) {
-				selectedUserType = uberType[i].id;
-				break;
-			}
-		}
-
-		if (selectedUserType === "uberSUV") {
-			kmNumberTypeBegin = kmNumberTypeBegin + 1000;
-			kmNumberTypeMiddle = kmNumberTypeMiddle + 2000;
-			kmNumberTypeExtend = kmNumberTypeExtend + 2000;
-			staticTime = staticTime + 1000;
-		} else if (selectedUserType === "uberBlack") {
-			kmNumberTypeBegin = kmNumberTypeBegin + 2000;
-			kmNumberTypeMiddle = kmNumberTypeMiddle + 4000;
-			kmNumberTypeExtend = kmNumberTypeExtend + 4000;
-			staticTime = staticTime + 2000;
-		}
-
-		if (0 < kmNumberVal && kmNumberVal <= 1) {
-			sumCharging = kmNumber * kmNumberTypeBegin + staticTime;
-		} else if (1 < kmNumberVal && kmNumberVal <= 20) {
-			sumCharging = kmNumber * kmNumberTypeMiddle + staticTime;
+		if (isNaN(kmNumberVal) || isNaN(timeWaitingVal)) {
+			chargingBlock.style.display = "block";
+			chargingInfo.innerText = "Vui lòng nhập số!";
 		} else {
-			sumCharging = kmNumber * kmNumberTypeExtend + staticTime;
-		}
+			kmNumber = parseFloat(kmNumberVal);
+			timeWaiting = parseFloat(timeWaitingVal);
 
-		chargingBlock.style.display = "block";
-		chargingInfo.innerText = sumCharging;
+			// Set uberX is checked as default
+			// - So number Payment is caculate following uberX
+			var kmNumberTypeBegin = 8000;
+			var kmNumberTypeMiddle = 12000;
+			var kmNumberTypeExtend = 10000;
+
+			var staticTime = timeWaiting * 2000;
+
+			var selectedUserType = "";
+			for (var i = 0; i < uberType.length; i++) {
+				if (uberType[i].checked) {
+					selectedUserType = uberType[i].id;
+					break;
+				}
+			}
+
+			if (selectedUserType === "uberSUV") {
+				kmNumberTypeBegin = kmNumberTypeBegin + 1000;
+				kmNumberTypeMiddle = kmNumberTypeMiddle + 2000;
+				kmNumberTypeExtend = kmNumberTypeExtend + 2000;
+				staticTime = staticTime + 1000;
+			} else if (selectedUserType === "uberBlack") {
+				kmNumberTypeBegin = kmNumberTypeBegin + 2000;
+				kmNumberTypeMiddle = kmNumberTypeMiddle + 4000;
+				kmNumberTypeExtend = kmNumberTypeExtend + 4000;
+				staticTime = staticTime + 2000;
+			}
+
+			if (0 < kmNumber && kmNumber <= 1) {
+				sumCharging = kmNumber * kmNumberTypeBegin + staticTime;
+			} else if (1 < kmNumber && kmNumber <= 20) {
+				sumCharging = kmNumber * kmNumberTypeMiddle + staticTime;
+			} else {
+				sumCharging = kmNumber * kmNumberTypeExtend + staticTime;
+			}
+
+			chargingBlock.style.display = "block";
+			chargingInfo.innerText = sumCharging + " vnd";
+		}
 	});
 }
 
 function handlePrintBill() {
 	document.getElementById("btnPrintBill").addEventListener("click", function() {
 		var billDetail = document.getElementById("billDetail");
+		var getInfoCharging = handleSumCharge.kmNumberVal;
+
+		console.log(getInfoCharging);
 		billDetail.innerHTML = "Bill detail here";
 	});
 }
 
-handleSumCharging();
+handleSumCharge();
 handlePrintBill();
