23e677006f588d53e639842cc955919a928c4b75
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-04-28 15:56:58 +0700

    [Js]: Implement charging with happy case for all Uber type

--- js/main.js
+++ js/main.js
@@ -1,37 +1,56 @@
-// var btncharging = document.getElementById("btncharging");
-// var btnPrintBill = document.getElementById("btnPrintBill");
-
-document.getElementById("btncharging").addEventListener("click", function () {
-  handleSumCharging();
-});
-
 function handleSumCharging() {
-  var uberType = document.getElementsByName("selector");
-  var uberX = document.getElementById("uberX");
-  var uberSUV = document.getElementById("uberSUV");
-  var uberBlack = document.getElementById("uberBlack");
-  var kmNumber = document.getElementById("kmNumber");
-  var timeWaiting = document.getElementById("timeWaiting");
-  var chargingInfoBlock = document.getElementById("divThanhTien");
-  console.log(chargingInfoBlock);
-  var chargingInfo = document.querySelector("#divThanhTien #xuatTien");
+	document.getElementById("btncharging").addEventListener("click", function () {
+		var kmNumber      = document.getElementById("kmNumber");
+		var timeWaiting   = document.getElementById("timeWaiting");
+		var chargingBlock = document.getElementById("divThanhTien");
+		var chargingInfo  = document.querySelector("#divThanhTien #xuatTien");
+		var uberType      = document.querySelectorAll('input[name="selector"]');
 
-  kmNumberVal = kmNumber.value;
-  timeWaitingVal = timeWaiting.value;
+		kmNumberVal = kmNumber.value;
+		timeWaitingVal = timeWaiting.value;
 
-  kmNumber = parseFloat(kmNumberVal);
-  timeWaiting = parseFloat(timeWaitingVal);
+		kmNumber = parseFloat(kmNumberVal);
+		timeWaiting = parseFloat(timeWaitingVal);
 
-  staticTime = timeWaiting * 2000;
+		// Set uberX is checked as default
+		// - So number Payment is caculate following uberX
+		var kmNumberTypeBegin = 8000;
+		var kmNumberTypeMiddle = 12000;
+		var kmNumberTypeExtend = 10000;
 
-  if (0 < kmNumberVal && kmNumberVal <= 1) {
-    sumCharging = kmNumber * 8000 + staticTime;
-  } else if (1 < kmNumberVal && kmNumberVal <= 20) {
-    sumCharging = kmNumber * 12000 + staticTime;
-  } else {
-    sumCharging = kmNumber * 10000 + staticTime;
-  }
+		var staticTime = timeWaiting * 2000;
 
-  chargingInfoBlock.style.display = "block";
-  chargingInfo.innerText = sumCharging;
+		var selectedUserType = "";
+		for (var i = 0; i < uberType.length; i++) {
+			if (uberType[i].checked) {
+				selectedUserType = uberType[i].id;
+				break;
+			}
+		}
+
+		if (selectedUserType === "uberSUV") {
+			kmNumberTypeBegin = kmNumberTypeBegin + 1000;
+			kmNumberTypeMiddle = kmNumberTypeMiddle + 2000;
+			kmNumberTypeExtend = kmNumberTypeExtend + 2000;
+			staticTime = staticTime + 1000;
+		} else if (selectedUserType === "uberBlack") {
+			kmNumberTypeBegin = kmNumberTypeBegin + 2000;
+			kmNumberTypeMiddle = kmNumberTypeMiddle + 4000;
+			kmNumberTypeExtend = kmNumberTypeExtend + 4000;
+			staticTime = staticTime + 2000;
+		}
+
+		if (0 < kmNumberVal && kmNumberVal <= 1) {
+			sumCharging = kmNumber * kmNumberTypeBegin + staticTime;
+		} else if (1 < kmNumberVal && kmNumberVal <= 20) {
+			sumCharging = kmNumber * kmNumberTypeMiddle + staticTime;
+		} else {
+			sumCharging = kmNumber * kmNumberTypeExtend + staticTime;
+		}
+
+		chargingBlock.style.display = "block";
+		chargingInfo.innerText = sumCharging;
+	});
 }
+
+handleSumCharging();
