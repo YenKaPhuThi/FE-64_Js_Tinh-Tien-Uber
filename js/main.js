16b51198155f27713ecdbe09ebc6fc153e59c760
YenKaPhuThi <yenkaphu96pnv@gmail.com>
2021-05-06 17:47:11 +0700

    [Js]: This is the second way to handle more professional in Js

--- js/main.js
+++ js/main.js
@@ -1,88 +1,111 @@
-function handleSumCharge() {
-  document.getElementById("btncharge").addEventListener("click", function () {
-    var kmNumber = document.getElementById("kmNumber");
-    var timeWaiting = document.getElementById("timeWaiting");
-    var chargeBlock = document.getElementById("divThanhTien");
-    var chargeInfo = document.querySelector("#divThanhTien #xuatTien");
-    var uberType = document.querySelectorAll('input[name="selector"]');
+// Get element by id following this way to be sorter
+function getMyEleId(ele) {
+  return document.getElementById(ele);
+}
 
-    // Get value of Km and Time waiting
-    kmNumberVal = kmNumber.value;
-    timeWaitingVal = timeWaiting.value;
+// Get element by selector following this way to be sorter
+function getMyEleSelector(ele) {
+  return document.querySelector(ele);
+}
 
-    // Checkt Km Number Value or Time Waiting is empty => don't do anything
+// Handle get Uber type
+function GetUberType() {
+  var uberType = "";
+  var uberX = getMyEleId("uberX").checked;
+  var uberSUV = getMyEleId("uberSUV").checked;
+  var uberBlack = getMyEleId("uberBlack").checked;
+
+  if (uberX) {
+    uberType = "uberX";
+  } else if (uberSUV) {
+    uberType = "uberSUV";
+  } else if (uberBlack) {
+    uberType = "uberBlack";
+  }
+
+  return uberType;
+};
+
+// Handle Charge Cost
+function HandleCharge() {
+  getMyEleId("btncharge").addEventListener("click", function() {
+    var kmNumber = getMyEleId("kmNumber").value;
+    var timeWait = getMyEleId("timeWait").value;
+    var chargeBlock = getMyEleId("divThanhTien");
+    var chargeInfo = getMyEleSelector("#divThanhTien #xuatTien");
+
+    // Check Km Number Value or Time Waiting is empty => Don't do anything
     // - These fields are required
-    if (kmNumberVal.length == 0 || timeWaitingVal.length == 0) {
+    if (kmNumber.length == 0 || timeWait.length == 0) {
       chargeBlock.style.display = "block";
       chargeInfo.innerText = "Vui lòng không để trống!";
 
       return null;
     }
-	  
-    // Check value's inputted are not number	  
-    if (isNaN(kmNumberVal) || isNaN(timeWaitingVal)) {
+
+    // Check value's inputted is not number
+    if (isNaN(kmNumber) || isNaN(timeWait)) {
       chargeBlock.style.display = "block";
       chargeInfo.innerText = "Vui lòng nhập số!";
     } else {
-      kmNumber = parseFloat(kmNumberVal);
-      timeWaiting = parseFloat(timeWaitingVal);
+      // Covert to Float type to caculate correctly
+      kmNumber = parseFloat(kmNumber);
+      timeWait = parseFloat(timeWait);
 
-      // Set uberX is checked as default
-      var uberTypeSelected = "";
-      for (var i = 0; i < uberType.length; i++) {
-        if (uberType[i].checked) {
-          uberTypeSelected = uberType[i].id;
+      // Get Uber type
+      var uberType = GetUberType();
+
+      // Set UberX as default
+      var UBER_ZONE_BEGIN = 8000;
+      var UBER_ZONE_MIDDLE = 12000;
+      var UBER_ZONE_END = 10000;
+      var UBER_ZONE_TIME = 2000;
+
+      switch (uberType) {
+        case "uberSUV":
+          UBER_ZONE_BEGIN = UBER_ZONE_BEGIN + 1000;
+          UBER_ZONE_MIDDLE = UBER_ZONE_MIDDLE + 2000;
+          UBER_ZONE_END = UBER_ZONE_END + 2000;
+          UBER_ZONE_TIME = UBER_ZONE_TIME + 1000;
           break;
-        }
+        case "uberBlack":
+          UBER_ZONE_BEGIN = UBER_ZONE_BEGIN + 2000;
+          UBER_ZONE_MIDDLE = UBER_ZONE_MIDDLE + 4000;
+          UBER_ZONE_END = UBER_ZONE_END + 4000;
+          UBER_ZONE_TIME = UBER_ZONE_TIME + 2000;
+          break;
       }
 
-      var kmNumberZone1 = 8000;
-      var kmNumberZone2 = 12000;
-      var kmNumberZone3 = 10000;
-
-      timeWaiting = timeWaiting * 2000;
-
-      if (uberTypeSelected === "uberSUV") {
-        kmNumberZone1 = kmNumberZone1 + 1000;
-        kmNumberZone2 = kmNumberZone2 + 2000;
-        kmNumberZone3 = kmNumberZone3 + 2000;
-        timeWaiting = timeWaiting + 1000;
-      } else if (uberTypeSelected === "uberBlack") {
-        kmNumberZone1 = kmNumberZone1 + 2000;
-        kmNumberZone2 = kmNumberZone2 + 4000;
-        kmNumberZone3 = kmNumberZone3 + 4000;
-        timeWaiting = timeWaiting + 2000;
-      }
-
+      var chargeCost = 0;
       if (0 < kmNumber && kmNumber <= 1) {
-        sumCharging = kmNumber * kmNumberZone1 + timeWaiting;
+        chargeCost = (kmNumber * UBER_ZONE_BEGIN) + (timeWait * UBER_ZONE_TIME);
       } else if (1 < kmNumber && kmNumber <= 20) {
-        sumCharging = kmNumber * kmNumberZone2 + timeWaiting;
+        chargeCost = (1 * UBER_ZONE_BEGIN) + ((kmNumber - 1) * UBER_ZONE_MIDDLE) + (timeWait * UBER_ZONE_TIME);
       } else {
-        sumCharging = kmNumber * kmNumberZone3 + timeWaiting;
+        chargeCost = (1 * UBER_ZONE_BEGIN) + (19 * UBER_ZONE_MIDDLE) + ((kmNumber - 20) * UBER_ZONE_END) + (timeWait * UBER_ZONE_TIME);
       }
 
       chargeBlock.style.display = "block";
-      chargeInfo.innerText = sumCharging + " vnd";
+      chargeInfo.innerHTML = chargeCost;
     }
   });
 }
 
+// Handle print bill
 function handlePrintBill() {
   document
     .getElementById("btnPrintBill")
-    .addEventListener("click", function () {
-      var kmInputted =  document.querySelector("#billDetail #kmInputted");
-      var chargeDetail =  document.querySelector("#billDetail #chargeDetail");
-	  
-      // Get value of Km and Charge Info    
-      var kmNumberVal = document.getElementById("kmNumber").value;
-      var chargeInfoVal = document.getElementById("xuatTien").textContent;
+    .addEventListener("click", function() {
+      var kmNumber = getMyEleId("kmNumber").value;
+      var chargeInfo = getMyEleId("xuatTien").textContent;
+      var kmInputted = getMyEleSelector("#billDetail #kmInputted");
+      var chargeDetail = getMyEleSelector("#billDetail #chargeDetail");
 
-      kmInputted.innerHTML = kmNumberVal.length ? kmNumberVal : 0;
-      chargeDetail.innerHTML = chargeInfoVal.length ? chargeInfoVal : 0;
+      kmInputted.innerHTML = kmNumber.length ? kmNumber : 0;
+      chargeDetail.innerHTML = chargeInfo.length ? chargeInfo : 0;
     });
 }
 
-handleSumCharge();
+GetUberType();
+HandleCharge();
 handlePrintBill();
