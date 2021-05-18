3d06c4a5bac10eb93cf7a808e2c8773f002844a9
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-18 07:38:34 +0700

    [Js]: Format code and rename variables to be professional

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getMyEleSelector(ele) {
 }
 
 // Handle get Uber type
-function GetUberType() {
+function getUberType() {
   var uberType = "";
   var uberX = getMyEleId("uberX").checked;
   var uberSUV = getMyEleId("uberSUV").checked;
@@ -26,8 +26,8 @@ function GetUberType() {
   return uberType;
 }
 
-// Message for notification
-var messageNotice = [
+// Define error Messages
+var errorMessage = [
   "Vui lòng không để trống số Km!",
   "Vui lòng không để trống thời gian chờ!",
   "Vui lòng nhập Km là số!",
@@ -35,58 +35,58 @@ var messageNotice = [
 ];
 
 // Handle check input's value
-function CheckValueInput(fieldEle, noticeEle, indexMsg) {
+function checkValueInput(fieldEle, errorEle, indexMsg) {
   var fieldEle = getMyEleId(fieldEle);
-  var noticeEle = getMyEleId(noticeEle);
- 
-  noticeEle.style.display = "none";
- 
+  var errorEle = getMyEleId(errorEle);
+
+  errorEle.style.display = "none";
+
   if (fieldEle.value === "") {
-    noticeEle.style.display = "block";
-    noticeEle.innerHTML = messageNotice[indexMsg];
+    errorEle.style.display = "block";
+    errorEle.innerHTML = errorMessage[indexMsg];
   }
 }
- 
+
 // Handle check input's value type is number
-function CheckValueInputType(fieldEle, noticeEle, indexMsg) {
+function checkValueInputType(fieldEle, errorEle, indexMsg) {
   var fieldEle = getMyEleId(fieldEle);
-  var noticeEle = getMyEleId(noticeEle);
- 
+  var errorEle = getMyEleId(errorEle);
+
   // Incase input's value is empty => Don't check value type
   if (fieldEle.value === "") {
     return null;
   } else {
-    noticeEle.style.display = "none";
- 
+    errorEle.style.display = "none";
+
     if (isNaN(fieldEle.value)) {
-      noticeEle.style.display = "block";
-      noticeEle.innerHTML = messageNotice[indexMsg];
+      errorEle.style.display = "block";
+      errorEle.innerHTML = errorMessage[indexMsg];
     }
   }
 }
- 
+
 // Handle Calculate Cost Payment
-function HandleCalculateCost (kmEle, timeWaitEle) {
+function handleCalculateCost(kmEle, timeWaitEle) {
   var kmNumber = getMyEleId(kmEle).value;
   var timeWait = getMyEleId(timeWaitEle).value;
- 
+
   // Incase input's value is empty  => Don't calculate cost payment
   if (kmNumber === "" || timeWait === "") {
     return null;
   } else {
     // Get Uber type
-    var uberType = GetUberType();
- 
+    var uberType = getUberType();
+
     // Convert input's value to float
     kmNumber = parseFloat(kmNumber);
     timeWait = parseFloat(timeWait);
- 
+
     // Set UberX as default
     var UBER_ZONE_BEGIN = 8000;
     var UBER_ZONE_MIDDLE = 12000;
     var UBER_ZONE_END = 10000;
     var UBER_ZONE_TIME = 2000;
- 
+
     switch (uberType) {
       case "uberSUV":
         UBER_ZONE_BEGIN = UBER_ZONE_BEGIN + 1000;
@@ -101,42 +101,49 @@ function HandleCalculateCost (kmEle, timeWaitEle) {
         UBER_ZONE_TIME = UBER_ZONE_TIME + 2000;
         break;
     }
- 
+
     var chargeCost = 0;
     if (0 < kmNumber && kmNumber <= 1) {
-      chargeCost = (kmNumber * UBER_ZONE_BEGIN) + (timeWait * UBER_ZONE_TIME);
+      chargeCost = kmNumber * UBER_ZONE_BEGIN + timeWait * UBER_ZONE_TIME;
     } else if (1 < kmNumber && kmNumber <= 20) {
-      chargeCost = (1 * UBER_ZONE_BEGIN) + ((kmNumber - 1) * UBER_ZONE_MIDDLE) + (timeWait * UBER_ZONE_TIME);
+      chargeCost =
+        1 * UBER_ZONE_BEGIN +
+        (kmNumber - 1) * UBER_ZONE_MIDDLE +
+        timeWait * UBER_ZONE_TIME;
     } else {
-      chargeCost = (1 * UBER_ZONE_BEGIN) + (19 * UBER_ZONE_MIDDLE) + ((kmNumber - 20) * UBER_ZONE_END) + (timeWait * UBER_ZONE_TIME);
+      chargeCost =
+        1 * UBER_ZONE_BEGIN +
+        19 * UBER_ZONE_MIDDLE +
+        (kmNumber - 20) * UBER_ZONE_END +
+        timeWait * UBER_ZONE_TIME;
     }
- 
+
     return chargeCost;
   }
 }
- 
+
 // Handle Charge Cost Payment
-function HandleCharge() {
-  getMyEleId("btnCharge").addEventListener("click", function() {
+function handleCharge() {
+  getMyEleId("btnCharge").addEventListener("click", function () {
     var chargeBlock = getMyEleId("divThanhTien");
     var chargeInfo = getMyEleSelector("#divThanhTien #xuatTien");
- 
+
     // Check Km Number Value & Time Waiting is empty => Show message
     //- These fields are required
-    CheckValueInput("kmNumber", "kmNotice", 0);
-    CheckValueInput("timeWait", "timeWaitNotice", 1);
- 
+    checkValueInput("kmNumber", "kmError", 0);
+    checkValueInput("timeWait", "timeWaitError", 1);
+
     // Check input's value is number
-    CheckValueInputType("kmNumber", "kmNotice", 2);
-    CheckValueInputType("timeWait", "timeWaitNotice", 3);
- 
+    checkValueInputType("kmNumber", "kmError", 2);
+    checkValueInputType("timeWait", "timeWaitError", 3);
+
     chargeBlock.style.display = "block";
-    chargeInfo.innerHTML = HandleCalculateCost("kmNumber", "timeWait");
+    chargeInfo.innerHTML = handleCalculateCost("kmNumber", "timeWait");
   });
 }
 
 // Handle print bill
-function HandlePrintBill() {
+function handlePrintBill() {
   getMyEleId("btnPrintBill").addEventListener("click", function () {
     var kmNumber = getMyEleId("kmNumber").value;
     var chargeInfo = getMyEleId("xuatTien").textContent;
@@ -148,5 +155,5 @@ function HandlePrintBill() {
   });
 }
 
-HandleCharge();
-HandlePrintBill();
+handleCharge();
+handlePrintBill();
